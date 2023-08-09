b7c60f309c702ea8441efaea3376eb8adf54a331
Bruno José dos Santos Wogt <brunojwogt@gmail.com>
2023-08-09 18:36:31 -0300

    @bruw/12/rename-authenticated-session-controller (#13)
    Co-authored-by: Bruno <brunow@pop-os.localdomain>

--- app/Http/Controllers/Auth/AuthenticatedTokenController.php
+++ app/Http/Controllers/Auth/AuthenticatedTokenController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Resources\UserResource;
+
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
 
-class AuthenticatedSessionController extends Controller
+class AuthenticatedTokenController extends Controller
 {
     /**
      * Handle an incoming authentication request.
