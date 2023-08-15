2f0c536e6d41fd5a5c691ecea2b21d2f7e4db167
Bruno <brunow@pop-os.localdomain>
2023-08-14 21:51:22 -0300

    added feature to update user

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\UserController;
-
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::controller(UserController::class)->group(function () {
         Route::get('/user', 'currentUser');
+        Route::put('/user', 'update');
     });
 });
 
