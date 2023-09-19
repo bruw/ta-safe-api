0950ddd4bfa3f1c7657c9753e4d600423dcd5dc7
Bruno <bruno@pop-os.localdomain>
2023-09-19 19:48:26 -0300

    created user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            UserSeeder::class,
             BrandSeeder::class,
             DeviceModelSeeder::class
         ]);
