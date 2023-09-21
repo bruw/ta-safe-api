7314a52a9ff2f9d5a9515241c10d313d044d24cf
Bruno <bruno@pop-os.localdomain>
2023-09-21 19:47:47 -0300

    refactoring the user seeder test

--- tests/Feature/Seeders/UserSeederTest.php
+++ tests/Feature/Seeders/UserSeederTest.php
@@ -12,6 +12,8 @@ class UserSeederTest extends TestCase
 {
     use RefreshDatabase;
 
+    private array $data;
+
     protected function setUp(): void
     {
         parent::SetUp();
@@ -19,22 +21,19 @@ class UserSeederTest extends TestCase
         $this->seed([
             UserSeeder::class
         ]);
+
+        $json = File::get(database_path('data/users.json'));
+        $this->data = json_decode($json);
     }
 
     public function test_must_have_created_the_correct_number_of_users(): void
     {
-        $json = File::get(database_path('data/users.json'));
-        $data = json_decode($json);
-
-        $this->assertEquals(User::count(), count($data));
+        $this->assertEquals(User::count(), count($this->data));
     }
 
     public function test_the_users_attributes_must_have_been_generated_correctly(): void
     {
-        $json = File::get(database_path('data/users.json'));
-        $data = json_decode($json);
-
-        foreach ($data as $item) {
+        foreach ($this->data as $item) {
             $user = User::where(['name' => $item->name])->first();
 
             $this->assertNotNull($user);
