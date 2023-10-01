9967f911f30f50b02086c3d8389fd3be2ccef354
Bruno <bruno@pop-os.localdomain>
2023-10-01 10:17:50 -0300

    refactoring to change the return to an http response

--- app/Http/Controllers/Device/DeviceSharingController.php
+++ app/Http/Controllers/Device/DeviceSharingController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Device;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\Device\DeviceResource;
 use App\Models\Device;
+use Illuminate\Http\Response;
 
 class DeviceSharingController extends Controller
 {
@@ -12,15 +12,13 @@ class DeviceSharingController extends Controller
      * Create token to share device registration.
      * 
      * @param \App\Models\Device $device
-     * @return \App\Http\Resources\Device\DeviceResource
+     * @return \Illuminate\Http\Response
      */
-    public function createSharingToken(Device $device): DeviceResource
+    public function createSharingToken(Device $device): Response
     {
         $this->authorize('createSharingToken', $device);
         $device->createSharingToken();
 
-        $device->refresh();
-
-        return new DeviceResource($device);
+        return response()->noContent(Response::HTTP_CREATED);
     }
 }
