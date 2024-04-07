22d4dac09e168d001bad88d014116f5eb69d7d22
bruw <brunojwogt@gmail.com>
2024-04-07 11:01:51 -0300

    changing the error message

--- lang/pt_BR/validation.php
+++ lang/pt_BR/validation.php
@@ -171,7 +171,7 @@ return [
         'device_transfer' => [
             'not_yourself' => 'Não é possível criar uma transferência de dispositivo para si mesmo',
             'in_progress' => 'Já existe uma transferência em andamento ainda não finalizada para este dispositivo.',
-            'register_not_validated' => 'Não é possível criar uma transferência para este dispositivo, pois o mesmo não foi validado.',
+            'register_not_validated' => 'Não é possível realizar transferências de registros não validados.',
             'transfer_closed' => 'Não é possível alterar uma proposta de transferência finalizada.',
             'unable_to_create_transfer' => 'Houve um problema ao criar a transferência do dispositivo. '
                 .' Por favor, verifique se as informações estão corretas e tente novamente em breve.',
