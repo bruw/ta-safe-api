d0c154892a2b0befd1366e6436ec4c90d24b1976
bruw <brunojwogt@gmail.com>
2024-05-02 10:57:20 -0300

    created new enum status in DeviceValidationStatus

--- app/Enums/Device/DeviceValidationStatus.php
+++ app/Enums/Device/DeviceValidationStatus.php
@@ -5,6 +5,7 @@ namespace App\Enums\Device;
 enum DeviceValidationStatus: string
 {
     case PENDING = 'pending';
+    case IN_ANALYSIS = 'in_analysis';
     case REJECTED = 'rejected';
     case VALIDATED = 'validated';
 }
