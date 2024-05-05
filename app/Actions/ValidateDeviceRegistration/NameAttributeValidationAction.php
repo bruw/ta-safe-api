012896c3b5608f5de834703d06bf14291b97ab75
bruw <brunojwogt@gmail.com>
2024-05-05 19:54:07 -0300

    action created to validate the owner name attribute of the device

---
+++ app/Actions/ValidateDeviceRegistration/NameAttributeValidationAction.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Actions\ValidateDeviceRegistration;
+
+use App\Constants\DeviceAttributeValidationRatio;
+use App\Models\Device;
+use App\Models\DeviceAttributeValidationLog;
+use App\Traits\StringNormalizer;
+use Exception;
+use FuzzyWuzzy\Fuzz;
+
+class NameAttributeValidationAction
+{
+    use StringNormalizer;
+
+    private Fuzz $fuzz;
+    private string $deviceUserName;
+    private string $invoiceConsumerName;
+    private DeviceAttributeValidationLog $result;
+    private const MIN_NAME_SIMILARITY =
+        DeviceAttributeValidationRatio::MIN_NAME_SIMILARITY;
+
+    public function __construct(
+        private Device $device,
+    ) {
+        $this->fuzz = new Fuzz();
+
+        $this->deviceUserName = $this->normalizeName(
+            $this->device->user->name
+        );
+
+        $this->invoiceConsumerName = $this->normalizeName(
+            $this->device->invoice->consumer_name
+        );
+    }
+
+    /**
+     * Run the validation process.
+     */
+    public function execute(): DeviceAttributeValidationLog
+    {
+        try {
+            $similarityRatio = $this->calculateRatio();
+            $this->persistResult($similarityRatio);
+        } catch (Exception $e) {
+            $this->persistResult(0);
+        } finally {
+            return $this->result;
+        }
+    }
+
+    /**
+     * Normalizes the attribute name by removing accents, digits and special characters.
+     */
+    private function normalizeName(string $name): string
+    {
+        return $this->extractOnlyLetters($name);
+    }
+
+    /**
+     * Returns the ratio score between userCpf and consumerCpf.
+     */
+    private function calculateRatio(): int
+    {
+        return $this->fuzz->ratio(
+            $this->deviceUserName,
+            $this->invoiceConsumerName,
+        );
+    }
+
+    /**
+     * Persists the results in the database.
+     */
+    private function persistResult($similarityRatio): void
+    {
+        $validated = $similarityRatio >= self::MIN_NAME_SIMILARITY;
+
+        $this->result = DeviceAttributeValidationLog::create([
+            'user_id' => $this->device->user->id,
+            'device_id' => $this->device->id,
+            'attribute_context' => get_class($this->device->user),
+            'attribute_name' => 'name',
+            'attribute_value' => $this->deviceUserName,
+            'provided_value' => $this->invoiceConsumerName,
+            'similarity_ratio' => $similarityRatio,
+            'min_similarity_ratio' => self::MIN_NAME_SIMILARITY,
+            'validated' => $validated,
+        ]);
+    }
+}
