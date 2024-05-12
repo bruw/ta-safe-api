d9ebe297d05649a3a0e86cd3ab4f565347c9d0c3
bruw <brunojwogt@gmail.com>
2024-05-12 10:11:40 -0300

    validation created on device model name

--- app/Constants/DeviceAttributeValidationRatio.php
+++ app/Constants/DeviceAttributeValidationRatio.php
@@ -13,4 +13,9 @@ class DeviceAttributeValidationRatio
      * Brand name similarity must be equal to or greater than 75%.
      */
     public const MIN_BRAND_SIMILARITY = 75;
+
+    /**
+     * Device model name similarity must be equal to or greater than 85%.
+     */
+    public const MIN_MODEL_NAME_SIMILARITY = 85;
 }
