d4ff30b5f44635575f119fe553eca51ee501dafb
bruw <bwogt.dev@gmail.com>
2025-07-23 13:26:04 -0300

    Added helpers to enums

--- app/Enums/Device/DeviceTransferStatus.php
+++ app/Enums/Device/DeviceTransferStatus.php
@@ -8,4 +8,44 @@ enum DeviceTransferStatus: string
     case REJECTED = 'rejected';
     case CANCELED = 'canceled';
     case ACCEPTED = 'accepted';
+
+    /**
+     * Retrieve an array of all the string values for each case in the enum.
+     */
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+
+    /**
+     * Check if the transfer status is pending.
+     */
+    public function isPending(): bool
+    {
+        return $this->value === self::PENDING->value;
+    }
+
+    /**
+     * Check if the transfer status is rejected.
+     */
+    public function isRejected(): bool
+    {
+        return $this->value === self::REJECTED->value;
+    }
+
+    /**
+     * Check if the transfer status is canceled.
+     */
+    public function isCanceled(): bool
+    {
+        return $this->value === self::CANCELED->value;
+    }
+
+    /**
+     * Check if the transfer status is accepted.
+     */
+    public function isAccepted(): bool
+    {
+        return $this->value === self::ACCEPTED->value;
+    }
 }
--- app/Enums/Device/DeviceValidationStatus.php
+++ app/Enums/Device/DeviceValidationStatus.php
@@ -8,4 +8,45 @@ enum DeviceValidationStatus: string
     case IN_ANALYSIS = 'in_analysis';
     case REJECTED = 'rejected';
     case VALIDATED = 'validated';
+
+    /**
+     * Retrieve an array of all the string values for each case in the enum.
+     */
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+
+    /**
+     * Check if the device validation status is pending.
+     */
+    public function isPending(): bool
+    {
+        return $this->value === self::PENDING->value;
+    }
+
+    /**
+     * Check if the device validation status is in analysis.
+     */
+    public function isInAnalysis(): bool
+    {
+        return $this->value === self::IN_ANALYSIS->value;
+    }
+
+    /**
+     * Check if the device validation status is rejected.
+     */
+    public function isRejected(): bool
+    {
+        return $this->value === self::REJECTED->value;
+    }
+
+    /**
+     * Check if the device validation status is validated.
+     */
+    public function isValidated(): bool
+    {
+        return $this->value === self::VALIDATED->value;
+    }
+
 }
