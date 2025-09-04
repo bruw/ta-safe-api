ebe5f7242c19302b6c462b59c4ae821eb97800b0
bruw <bwogt.dev@gmail.com>
2025-09-04 10:17:37 -0300

    The device imei validation feature has been refactored

---
+++ app/Actions/DeviceInvoice/Validation/Imei/DeviceImei1ValidationAction.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Actions\DeviceInvoice\Validation\Imei;
+
+use App\Actions\DeviceInvoice\Validation\Base\BaseDeviceProductValidationAction;
+use App\Constants\DeviceAttributeValidationRatio;
+use App\Models\Device;
+use App\Utils\StringNormalize;
+
+class DeviceImei1ValidationAction extends BaseDeviceProductValidationAction
+{
+    /**
+     * Returns the given value normalized.
+     */
+    protected function normalize(string $value): string
+    {
+        return StringNormalize::for($value)
+            ->keepOnlyDigits()
+            ->removeExtraWhiteSpaces()
+            ->get();
+    }
+
+    /**
+     * Returns the device attribute to validate.
+     */
+    protected function deviceAttributeToValidate(): string
+    {
+        return $this->device->imei_1;
+    }
+
+    /**
+     * Returns the minimum similarity ratio for this validation type.
+     */
+    protected function minSimilarityRatio(): int
+    {
+        return DeviceAttributeValidationRatio::MIN_IMEI_SIMILARITY;
+    }
+
+    /**
+     * Returns the source of the attribute to validate.
+     */
+    protected function attributeSource(): string
+    {
+        return Device::class;
+    }
+
+    /**
+     * Returns the label of the attribute to validate.
+     */
+    protected function attributeLabel(): string
+    {
+        return 'imei_1';
+    }
+}
